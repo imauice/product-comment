a62f889ec676f15dda0d901179e3a65da81dfee7
imauice <lxauicexl@gmail.com>
2023-09-16 20:48:27 +0700

    update signin and signout page

--- vue-starter/src/views/SigninView.vue
+++ vue-starter/src/views/SigninView.vue
@@ -1,46 +1,59 @@
 <template>
-    <div class="flex flex-column justify-item-center ">
-        <h1 class="display-6">Wecome to Imauice starter project </h1>
-        <p>please signin to look around this template</p>
-        <h3 class="mb-3">Sign In</h3>
-        <input v-model="username" type="text" style="max-width:15rem" class="form-control mb-2" placeholder="Username"/>
-        <input v-model="password" type="password" style="max-width:15rem" class="form-control" placeholder="Password"/>
-        <button class="btn btn-primary my-3" @click="SignIn">Signin</button>
-        <span class="mx-2" @click="$router.push('/signup')">signup</span>
+    <div >
+        <div class="signin-panel d-flex justify-content-center w-100 align-items-center mt-3">
+            <div class="text-light">
+                
+                <h1 class="display-6 mb-3">Sign In</h1>
+                <input v-model="username" type="text" style="max-width:15rem" class="form-control mb-2"
+                placeholder="Username" />
+                <input v-model="password" type="password" style="max-width:15rem" class="form-control" placeholder="Password" />
+                <button class="btn btn-primary my-3" @click="SignIn">Signin</button>
+                <span class="mx-2" @click="$router.push('/signup')">signup</span>
+            </div>
         </div>
+    </div>
 </template>
 <script>
 import User from "@/services/user";
 
 export default {
-setup(){
-    const userservice = new User();
-    return { userservice }
-},
-data(){
-    return {
-        username:"",
-        password:""
-    }
-},
-methods:{
-    async SignIn(){
-
-        const logindata = {
-            username:this.username,
-            password:this.password
+    setup() {
+        const userservice = new User();
+        return { userservice }
+    },
+    data() {
+        return {
+            username: "",
+            password: ""
         }
+    },
+    methods: {
+        async SignIn() {
 
-        await this.userservice.Signin(logindata).then(result => {
-            if(result){
-                this.$router.push('/profile');
+            const logindata = {
+                username: this.username,
+                password: this.password
             }
-        })
+
+            await this.userservice.Signin(logindata).then(result => {
+                if (result) {
+                    this.$router.push('/profile');
+                }
+            })
+        }
     }
-}
 
 }
 </script>
 <style scoped>
+
+.card{
+  padding: 5rem;
+   
+}
+
+.signin-panel{
+    min-height: 70vh;
+}
 
 </style>
--- vue-starter/src/views/SignupView.vue
+++ vue-starter/src/views/SignupView.vue
@@ -1,15 +1,17 @@
 <template>
-    <div>
+    <div class="signup d-flex justify-content-center align-items-center text-light">
+<div>
 
-        <h1 class="display-6 mb-3">Signup</h1>
-        <label>username</label>
-        <input v-model="username" type="text" class="form-control mb-3" style="max-width:15rem"/>
-        <label>password</label>
-        <input v-model="password" type="password" class="form-control mb-3" style="max-width:15rem"/>
-        <label>email</label>
-        <input v-model="email" type="email" class="form-control mb-3" style="max-width:15rem"/>
-        <button class="btn btn-primary" @click="SignUp">Sign Up</button>
-    </div>
+    <h1 class="display-6 mb-3">Signup</h1>
+    <label>username</label>
+    <input v-model="username" type="text" class="form-control" style="max-width:15rem"/>
+    <label>password</label>
+    <input v-model="password" type="password" class="form-control" style="max-width:15rem"/>
+    <label>email</label>
+    <input v-model="email" type="email" class="form-control" style="max-width:15rem"/>
+    <button class="btn btn-primary my-3" @click="SignUp">Sign Up</button>
+</div>
+</div>
 
 </template>
 <script>
@@ -44,3 +46,8 @@ export default {
     }
 }
 </script>
+<style scoped>
+.signup{
+    min-height: 70vh;
+}
+</style>
